b3b381e0932aed95a273e94e16a9ec6e8058a380
BombingArt <74514537+BombingArt@users.noreply.github.com>
2024-02-11 03:17:01 +0200

    Update main.py

--- task_4/main.py
+++ task_4/main.py
@@ -3,29 +3,39 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
+
 
 def change_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact changed."
 
+
 def show_contact(args, contacts):
     name = args[0]
     return contacts[name]
 
+
 def all_contacts(contacts):
-    for name, phone in contacts.items():
-        print(f"Name: {name}, phone: {phone}.")
+    contact_strings = []
+    if contacts:
+        for name, phone in contacts.items():
+            contact_strings.append(f"Name: {name}, phone: {phone}.")
+    else:
+        contact_strings.append("No contacts available.")
+    return contact_strings
+
 
 def main():
-    
+
     contacts = {}
     print("Welcome to the assistant bot!")
-    
+
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
@@ -36,21 +46,24 @@ def main():
 
         elif command == 'hello':
             print("How can I help you?")
-        
+
         elif command == 'add':
             print(add_contact(args, contacts))
-        
+
         elif command == 'change':
             print(change_contact(args, contacts))
-        
+
         elif command == 'phone':
             print(show_contact(args, contacts))
 
         elif command == 'all':
-            all_contacts(contacts)
-        
+            contacts_strings = all_contacts(contacts)
+            for contact_string in contacts_strings:
+                print(contact_string)
+
         else:
             print("Invalid command.")
+
 
 if __name__ == '__main__':
     main()
